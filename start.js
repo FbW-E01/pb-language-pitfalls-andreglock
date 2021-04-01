fd5a6ac472062b19c2199f0a858ff88bc0c57190
André <andreglock@gmail.com>
2021-04-01 13:09:41 +0200

    done

---
+++ start.js
@@ -0,0 +1,15 @@
+// NaN stands for Not a Number and it's a type of number
+
+console.log('Question 1 - NaN:');
+console.log(typeof "hello" === NaN);
+console.log(typeof 3 === NaN);
+console.log(typeof NaN);
+
+console.log('Question 2 - Rounding:');
+console.log(0.1 * 0.2);
+console.log((0.1 * 0.2).toFixed(2));
+
+console.log('Question 3 - Infinity:');
+console.log(Infinity / 0);
+console.log(Infinity / Infinity);
+console.log(1 / 0);
